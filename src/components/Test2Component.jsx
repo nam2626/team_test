e27c487178ed3b5e121ab80402e5c570634500c2
TEEPUT <ciyi1594@gmail.com>
2024-06-05 17:33:09 +0900

    4조 수정입니다~

--- src/components/Test2Component.jsx
+++ src/components/Test2Component.jsx
@@ -2,4 +2,9 @@ import TestComponent from './TestComponent'
 
 export default function Test2Component() {
     <TestComponent />
+    return (
+        <>
+            <h1> 안녕하세요 ~ 4조입니다 ~ </h1>
+        </>
+    )
 }
